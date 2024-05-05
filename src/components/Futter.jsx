69e4229a5317caf7ba75b2949919449400dc9d5e
Mohd Bilal <bilalmail7054@gmail.com>
2024-05-05 21:39:42 +0530

    Added footer

--- src/components/Futter.jsx
+++ src/components/Futter.jsx
@@ -1,7 +1,9 @@
+import { Link } from "@phosphor-icons/react";
+
 export default function Futter(){
     return(
-        <section className="absolute bottom-0 w-full flex justify-center">
-    Copyrights &copy; {new Date().getFullYear()}
+        <section className="mt-4 w-full flex justify-center">
+    Copyrights <a href="https://webweaver-personalportfolio.netlify.app/" className="font-semibold mx-2 flex  hover:text-white transition-all" target="_blank"> Webweaver<Link size={25} weight="duotone" /> </a> &copy; {new Date().getFullYear()}
 </section>
     );
 }
