c2024f372b745e8060c2c6ba4ecc97048a0f36d6
Mohd Bilal <bilalmail7054@gmail.com>
2024-05-20 01:26:51 +0530

    dfsdff

--- src/components/Futter.jsx
+++ src/components/Futter.jsx
@@ -2,7 +2,7 @@ import { Link } from "@phosphor-icons/react";
 
 export default function Futter(){
     return(
-        <section className="mt-4 w-full flex justify-center">
+        <section className="mt-4 w-full flex fixed bottom-0 justify-center">
     Copyrights <a href="https://webweaver-personalportfolio.netlify.app/" className="font-semibold mx-2 flex  hover:text-white transition-all" target="_blank"> Webweaver<Link size={25} weight="duotone" /> </a> &copy; {new Date().getFullYear()}
 </section>
     );
